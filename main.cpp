f3675d3cc0454af0fe06b7a991a9f8e066a7af2c
Szabó Attila <neyo8826@gmail.com>
2016-06-17 16:26:44 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <asio.hpp>
+#include <iostream>
+#include <memory>
+#include <thread>
+
+int main()
+{
+	try
+	{
+		asio::io_service ioService;
+
+		auto threadPoolSize = std::thread::hardware_concurrency();
+		std::vector<std::unique_ptr<std::thread>> threadPool{threadPoolSize};
+		for (auto& thread : threadPool)
+			thread = std::make_unique<std::thread>([&]{ ioService.run(); });
+		ioService.run();
+		for (auto& thread : threadPool)
+			thread->join();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
